881aade645eb8fec14b8c6111da2a2f4bcceedde
Jimmy D <jd9192016@gmail.com>
2019-06-28 10:41:51 -0400

    Complete(1st peer review)

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Is a Porfolio app built using mainly Node.js
 
 
+error.pug display is displayed when request is routed to a non-existent route.
+
+Added CSS styles
+    -background color change
+    -Font-size change
+    -Transition Added
+    . box shadows added
---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const pug = require('pug');
+const app = express();
+app.use('/static', express.static('public'))
+app.use(express.static('images'))
+const data = require('./data.json').projects;
+app.set('view engine', 'pug');
+
+
+app.get('/', (req, res) => {
+  res.render('index', {projects:data});
+})
+
+app.get('/about', (req, res) => {
+  res.render('about');
+})
+
+app.get('/project:id', (req, res) => {
+    const id = req.params.id;
+    res.render('project', {project:data[id]});
+})
+
+app.use((req, res, next) => {
+  const err = new Error('Page Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) =>{
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
+});
+
+app.listen(3000,console.log("Server running on port 3000!"));
