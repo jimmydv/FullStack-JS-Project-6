2055a41dcd5f66d410395f327670e60122da568e
Jimmy D <jd9192016@gmail.com>
2019-07-01 01:17:25 -0400

    Error handling updated
    Conditional added on the project:id route so it throws an error if wrong id is imputed

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const data = require('./data.json').projects;
 app.set('view engine', 'pug');
 
 
+
 app.get('/', (req, res) => {
   res.render('index', {projects:data});
 })
@@ -16,21 +17,35 @@ app.get('/about', (req, res) => {
   res.render('about');
 })
 
-app.get('/project:id', (req, res) => {
+app.get('/project:id', (req, res, next) => {
     const id = req.params.id;
+    if(data[id]===undefined){
+      const err = new Error("Oops! Sorry something went wrong.");
+      console.log(err);
+      err.status = 500;
+      next(err);
+    }else{
     res.render('project', {project:data[id]});
+  }
 })
-
+// Routing Error Hanlding 404
 app.use((req, res, next) => {
   const err = new Error('Page Not Found');
   err.status = 404;
-  console.log(err.message)
   next(err);
 });
 
+// app.use((req, res, next) => {
+//  const  err = new Error("Oops! Sorry something went wrong.");
+//   console.log("Oops! Sorry something went wrong.")
+//   err.status = 500;
+//   next(err);
+// });
+
+
 app.use((err, req, res, next) =>{
   res.locals.error = err;
-  res.status(err.status);
+  res.status; // pick the status from the error handling middlewears if there is an error. No need to pass the value.
   res.render('error');
 });
 
